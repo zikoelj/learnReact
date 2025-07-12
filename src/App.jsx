b27000387a7dadc114c92f43c8b65717946947b8
zikoelj <zakariaelj24@gmail.com>
2025-07-12 06:54:25 +0100

    conditional rendering by making lists reusable component & style them & ternary operator to display in case if there are items in lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import List from "./List.jsx"
 function App(){
+    const fruits = [{id : 1, name :"Apple",calories : 95},
+                    {id : 2, name :"Orange",calories : 45},
+                    {id : 3 , name :"banana",calories : 105},
+                    {id : 4, name :"coconut",calories : 159},
+                    {id : 5, name :"pineapple",calories : 37}]
+
+    const vegetables = [{id : 6, name :"potatos",calories : 110},
+                        {id : 7, name :"celery",calories : 15},
+                        {id : 8, name :"carrots",calories : 25},
+                        {id : 9, name :"corn",calories : 63},
+                        {id : 10, name :"broccoli",calories : 50}]
+     
     return(
-        <List/>
+        <>
+        {fruits.length > 0 ?<List items= {fruits} category="fruits"/> : null }
+        {vegetables.length > 0 ? <List items= {vegetables} category="vegetables"/> : null}
+        </>
     );
     }
 export default App
--- src/List.jsx
+++ src/List.jsx
@@ -1,25 +1,17 @@
 //rendering list in reactJs
-function List(){
+function List(props){
 
-    const fruits = [{id : 1, name :"Apple",calories : 95},
-                    {id : 2, name :"Orange",calories : 45},
-                    {id : 3, name :"banana",calories : 105},
-                    {id : 4, name :"coconut",calories : 159},
-                    {id : 5, name :"pineapple",calories : 37}]
-
-    //fruits.sort((a,b)=>a.name.localeCompare(b.name)); //ALPHABETICAL
-    //fruits.sort((a,b)=>b.name.localeCompare(a.name)); //REVERSE ALPHABETICAL
-    //fruits.sort((a,b) => a.calories - b.calories); //NUMERIC
-    //fruits.sort((a,b) => b.calories - a.calories); //REVERSE NUMERIC
-    //const lowCalFruits = fruits.filter(fruit => fruit.calories <100);
-    const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
-    
-
-    const listItems = fruits.map(fruit => 
-                                        <li key={fruit.id} >
-                                          {fruit.name}: &nbsp;
-                                          <b>{fruit.calories}</b>
+    const category = props.category;
+    const itemList = props.items; //List is a reusable component
+    const listItems = itemList.map(item => 
+                                        <li key={item.id} >
+                                          {item.name}: &nbsp;
+                                          <b>{item.calories}</b>
                                         </li>)
-    return (<ol>{listItems}</ol>);
+    return (<>
+            <h3 className="list-category">{category}</h3>
+            <ol className="list-items">{listItems}</ol>
+            </>
+            );
 }
 export default List  
