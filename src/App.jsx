8d1e63b863401c0bf557fb7a39693d6ee9daba07
zikoelj <zakariaelj24@gmail.com>
2025-07-18 04:39:20 +0100

    updater function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-
+import Counter from "./Counter";
 function App(){
    
     return(<>
-
+<Counter/>
     
     </>);
        
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,11 +5,31 @@ function Counter(){
     const [count,setCount] = useState(0);
 
     const incrementCounter = () =>{
-        setCount(count + 1);
+        //using updater function ,its take the previous value
+        //of the count and apply to it the change
+        //however using setCount(count + 1) even three time 
+        // it will give us always 1 because this approach 
+        //based on the initial value of count
+        //asynchronous function setCount(count + 1)
+
+        //by the way for showing youre are using updater 
+        //function we take the first character of the state
+        //variable or "prev"NameStateVariable
+
+        setCount(count + 1); //count = 0 -> count = 1
+        setCount(count + 1); //count = 0 -> count = 1
+        setCount(count + 1); //count = 0 -> count = 1
+
+        setCount(c => c + 1); //c = 0 -> c = 1
+        setCount(c => c + 1); //c = 1 -> c = 2
+        setCount(c => c + 1); //c = 2 -> c = 3
     }
 
     const decrementCounter = () =>{
-        setCount(count - 1);
+        setCount(c => c - 1); //c = 0 -> c = -1
+        setCount(c => c - 1); //c = -1 -> c = -2
+        setCount(c => c - 1); //c = -2 -> c = -3
+
     }
 
     const resetCounter = () =>{
